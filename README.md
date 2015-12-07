8585b4cd4e5065663b83ede8e35dd42ea48a0048
Awilum <awilum@msn.com>
2015-12-07 20:24:25 +0300

    Init Commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# morfy-plugin-tiny
+# Tiny Url
+
+Tiny Url plugin takes a long url and uses the TinyURL API to return a shortened version.
+
+## Installation
+See [this instruction](http://morfy.org/documentation/plugins/plugins-installation)
+
+## Usage in page content
+
+Simple usage
+
+```
+{tiny url="http://morfy.org"}
+```
+
+## Usage in template
+
+```smarty
+{tiny('http://morfy.org')}
+```
+
+## Options
+
+| name  | value | description |
+|---|---|---|
+| enabled | true | or `false` to disable the plugin |
+
+## License
+See  [LICENSE](https://github.com/morfy-cms/morfy-plugin-tiny-url/blob/master/LICENSE)
---
+++ tiny-url.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Morfy Tiny Url Plugin
+ *
+ * (c) Romanenko Sergey / Awilum <awilum@msn.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// Shortcode: {tiny}
+Shortcode::add('tiny', function ($attributes) {
+
+    // Extract
+    extract($attributes);
+
+    return (isset($url)) ? tiny($url) : '';
+});
+
+/**
+ * Takes a long url and uses the TinyURL API to return a shortened version.
+ *
+ *  <code>
+ *      tiny('http:://sitename.com');
+ *  </code>
+ *
+ * @param  string $url Long url
+ * @return string
+ */
+function tiny($url)
+{
+    return (isset($url)) ? file_get_contents('http://tinyurl.com/api-create.php?url='.$url) : '';
+}
